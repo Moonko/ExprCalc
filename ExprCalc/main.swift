41bce5e296a90ebfaa06af8824b26276063b94ed
Moonko <moonko@me.com>
2015-02-16 01:29:58 +0300

    Initial Commit

---
+++ ExprCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExprCalc
+//
+//  Created by Андрей Рычков on 16.02.15.
+//  Copyright (c) 2015 Андрей Рычков. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
